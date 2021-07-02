b8b5640ad15b8cbafd0601191e3fb93043690b6e
brandon tuttle <tuttlebr@ncc-1701.biz>
2021-07-02 17:07:52 -0400

    added .gitignore

--- StyleTransfer_Server.py
+++ StyleTransfer_Server.py
@@ -5,11 +5,13 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class DoTransfer(Resource):
     def get(self, content_image, style_image):
-        return {'data': StyleTransfer.st_model(content_image, style_image)}
+        return {"data": StyleTransfer.st_model(content_image, style_image)}
 
-api.add_resource(DoTransfer, '/generative/<content_image>/<style_image>')
 
-if __name__ == '__main__':
-     app.run()
+api.add_resource(DoTransfer, "/generative/<content_image>/<style_image>")
+
+if __name__ == "__main__":
+    app.run()
